11bbb5fb3b4e7134b90fdc22809730ff9bb9a2d8
Seth <matvmp7@gmail.com>
2018-03-30 18:17:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import time
 import os
 
 def bot_login():
-	reddit = praw.Reddit(username = config.username,
+	r = praw.Reddit(username = config.username,
 		password = config.password,
 		client_id = config.client_id,
 		client_secret = config.client_secret,
 		user_agent = "Bruh Bot")
-	return reddit
+	return r
 
-def run_bot(reddit, comments_replied_to):
+def run_bot(r, comments_replied_to):
 	print "Searching for Bruh comments"
 
-	for comment in reddit.subreddit('test').comments(limit=25):
-		if "bruh" in comment.body and comment.id not in comments_replied_to and not comment.author != reddit.user.me():
+	for comment in r.subreddit('askreddit').comments(limit=1000):
+		if "bruh" in comment.body and comment.id not in comments_replied_to:
 			print "Found \"Bruh\" in comment " + comment.id
 			comment.reply("[...athkayosaurus](https://en.wikipedia.org/wiki/Bruhathkayosaurus)")
 			print "Replied to comment " + comment.id 
@@ -24,17 +24,6 @@ def run_bot(reddit, comments_replied_to):
 
 			with open ("comments_replied_to.txt", "a") as f:
 				f.write(comment.id + "\n")
-
-	for comment in reddit.subreddit('askreddit').comments(limit=25):
-			if "bruh" in comment.body and comment.id not in comments_replied_to and not comment.author != reddit.user.me():
-				print "Found \"Bruh\" in comment " + comment.id
-				comment.reply("[...athkayosaurus](https://en.wikipedia.org/wiki/Bruhathkayosaurus)")
-				print "Replied to comment " + comment.id 
-
-				comments_replied_to.append(comment.id)
-
-				with open ("comments_replied_to.txt", "a") as f:
-					f.write(comment.id + "\n")
 
 	print "Sleeping for 10 seconds..."
 	time.sleep(10)
@@ -52,11 +41,10 @@ def get_saved_comments():
 	return comments_replied_to
 
 
-reddit = bot_login()
+r = bot_login()
 comments_replied_to = get_saved_comments()
-
+print comments_replied_to
 
 while True:
-	run_bot(reddit, comments_replied_to)
-
+	run_bot(r, comments_replied_to)
 
